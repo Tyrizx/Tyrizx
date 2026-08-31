0560248affb495069dbc2ebc7c7f152d5ab4c612
Frostre_ <n9043395@gmail.com>
2026-08-31 07:43:20 +0200

    Add Java and Kotlin compatibility options

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,11 +24,16 @@ android {
             )
         }
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
