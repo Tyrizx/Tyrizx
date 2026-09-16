8ff0f89442e38f6c280964707ad382545bf293ec
Frostre_ <n9043395@gmail.com>
2026-09-16 19:31:29 +0200

    Configure GitHub Packages repository in settings
    Added GitHub Packages repository for Optima.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,12 +5,22 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven {
+            name = "GitHubPackagesOptima"
+            url = uri("https://maven.pkg.github.com/SolaraStudio/Optima")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR") ?: project.findProperty("gpr.user") as String?
+                password = System.getenv("GITHUB_TOKEN") ?: project.findProperty("gpr.key") as String?
+            }
+        }
     }
 }
-rootProject.name = "rift"
+
+rootProject.name = "Tyrizx"
 include(":app")
