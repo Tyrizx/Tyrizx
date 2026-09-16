ffe04510f336db096fe6099d972e5615c0581b49
Frostre_ <n9043395@gmail.com>
2026-09-16 19:51:59 +0200

    Refactor GitHub credentials retrieval in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,8 +15,8 @@ dependencyResolutionManagement {
             name = "GitHubPackagesOptima"
             url = uri("https://maven.pkg.github.com/SolaraStudio/Optima")
             credentials {
-                username = System.getenv("GITHUB_ACTOR") ?: project.findProperty("gpr.user") as String?
-                password = System.getenv("GITHUB_TOKEN") ?: project.findProperty("gpr.key") as String?
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
             }
         }
     }
