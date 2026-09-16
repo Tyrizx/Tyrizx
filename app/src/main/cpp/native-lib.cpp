d1ae3de1a9a08c3df657dc001a73e1af2172fe9d
Frostre_ <n9043395@gmail.com>
2026-09-16 20:34:14 +0200

    Add JNI function to start Node with arguments

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,23 @@
+#include <jni.h>
+#include <string>
+#include <node.h>
+
+extern "C" JNIEXPORT void JNICALL
+Java_io_tyrizx_MainActivity_startNodeWithArguments(
+        JNIEnv* env,
+        jobject /* this */,
+        jobjectArray arguments) {
+
+    int argc = env->GetArrayLength(arguments);
+    char** argv = new char*[argc];
+
+    for (int i = 0; i < argc; i++) {
+        jstring string = (jstring) env->GetObjectArrayElement(arguments, i);
+        const char* rawString = env->GetStringUTFChars(string, 0);
+        argv[i] = strdup(rawString);
+        env->ReleaseStringUTFChars(string, rawString);
+        env->DeleteLocalRef(string);
+    }
+
+    node::Start(argc, argv);
+}
