ad77f64d10214ede6b1fd491e27679e65c4515b2
Frostre_ <n9043395@gmail.com>
2026-09-17 20:32:52 +0200

    Refactor build.gradle.kts for packaging and ndk
    Removed ndk abiFilters and updated packagingOptions to jniLibs.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,10 +14,6 @@ android {
         versionCode = 1
         versionName = "0.1"
 
-        ndk {
-            abiFilters += listOf("arm64-v8a", "armeabi-v7a", "x86_64")
-        }
-
         externalNativeBuild {
             cmake {
                 cppFlags += "-std=c++17"
@@ -41,9 +37,11 @@ android {
         }
     }
 
-    packagingOptions {
-        pickFirst("**/libnode.so")
-        pickFirst("**/libc++_shared.so")
+    packaging {
+        jniLibs {
+            pickFirsts.add("**/libnode.so")
+            pickFirsts.add("**/libc++_shared.so")
+        }
     }
 
     compileOptions {
