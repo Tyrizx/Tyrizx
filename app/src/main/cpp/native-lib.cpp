d1c24529f86ff4222edda2e1c617948f0e051a7a
Frostre_ <n9043395@gmail.com>
2026-09-18 02:27:35 +0200

    Update native-lib.cpp

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,17 +1,61 @@
 #include <jni.h>
 #include <string>
 #include <cstdlib>
+#include <pthread.h>
+#include <unistd.h>
+#include <android/log.h>
 #include "node.h"
 
+// --- Redirect stdout and stderr to logcat ---
+const char *ADBTAG = "NODEJS-MOBILE";
+int pipe_stdout[2];
+int pipe_stderr[2];
+pthread_t thread_stdout;
+pthread_t thread_stderr;
+
+void *thread_stderr_func(void*) {
+    ssize_t redirect_size;
+    char buf[2048];
+    while((redirect_size = read(pipe_stderr[0], buf, sizeof buf - 1)) > 0) {
+        if(buf[redirect_size - 1] == '\n') --redirect_size;
+        buf[redirect_size] = 0;
+        __android_log_write(ANDROID_LOG_ERROR, ADBTAG, buf);
+    }
+    return 0;
+}
+
+void *thread_stdout_func(void*) {
+    ssize_t redirect_size;
+    char buf[2048];
+    while((redirect_size = read(pipe_stdout[0], buf, sizeof buf - 1)) > 0) {
+        if(buf[redirect_size - 1] == '\n') --redirect_size;
+        buf[redirect_size] = 0;
+        __android_log_write(ANDROID_LOG_INFO, ADBTAG, buf);
+    }
+    return 0;
+}
+
+int start_redirecting_stdout_stderr() {
+    setvbuf(stdout, 0, _IONBF, 0);
+    pipe(pipe_stdout);
+    dup2(pipe_stdout[1], STDOUT_FILENO);
+    setvbuf(stderr, 0, _IONBF, 0);
+    pipe(pipe_stderr);
+    dup2(pipe_stderr[1], STDERR_FILENO);
+    if(pthread_create(&thread_stdout, 0, thread_stdout_func, 0) == -1) return -1;
+    pthread_detach(thread_stdout);
+    if(pthread_create(&thread_stderr, 0, thread_stderr_func, 0) == -1) return -1;
+    pthread_detach(thread_stderr);
+    return 0;
+}
+
 extern "C" JNIEXPORT jint JNICALL
-Java_io_tyrizx_MainActivity_startNodeWithArguments(
-        JNIEnv *env,
-        jobject /* this */,
-        jobjectArray arguments) {
+Java_io_tyrizx_MainActivity_startNodeWithArguments(JNIEnv *env, jobject, jobjectArray arguments) {
+    // Start redirecting stdout and stderr to logcat
+    start_redirecting_stdout_stderr();
 
     int argc = env->GetArrayLength(arguments);
     char** argv = new char*[argc];
-
     for (int i = 0; i < argc; i++) {
         jstring string = (jstring) env->GetObjectArrayElement(arguments, i);
         const char* rawString = env->GetStringUTFChars(string, 0);
@@ -19,6 +63,5 @@ Java_io_tyrizx_MainActivity_startNodeWithArguments(
         env->ReleaseStringUTFChars(string, rawString);
         env->DeleteLocalRef(string);
     }
-
     return node::Start(argc, argv);
 }
