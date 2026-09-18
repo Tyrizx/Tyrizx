9c10ebf4d688e6750d3bebef558c4e1ada48becd
Frostre_ <n9043395@gmail.com>
2026-09-18 02:15:00 +0200

    Update NDK version to 24.0.8215888

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 android {
     namespace = "io.tyrizx"
     compileSdk = 35
-    ndkVersion = "22.1.7171670"
+    ndkVersion = "24.0.8215888"g
 
     defaultConfig {
         applicationId = "io.tyrizx"
